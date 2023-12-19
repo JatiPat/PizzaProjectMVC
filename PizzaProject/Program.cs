7fd12c607294d0caad0992f76d57093cd0bbdddf
Jatin Patel <110480554+JatiPat@users.noreply.github.com>
2023-12-19 10:24:25 -0500

    dev branch created

--- PizzaProject/Program.cs
+++ PizzaProject/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
